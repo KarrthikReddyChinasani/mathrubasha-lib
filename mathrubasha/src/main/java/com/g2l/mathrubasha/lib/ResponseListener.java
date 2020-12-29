e8d0c326f719cad40b72bb641e63b8a16ac3b57b
khushi1706 <git-commit-address>
2020-12-29 21:59:33 +0530

    third commit

--- mathrubasha/src/main/java/com/g2l/mathrubasha/lib/ResponseListener.java
+++ mathrubasha/src/main/java/com/g2l/mathrubasha/lib/ResponseListener.java
@@ -1,5 +1,5 @@
 package com.g2l.mathrubasha.lib;
 
-public interface Callback {
+public interface ResponseListener {
     public  void onReceive(String s);
 }
--- mathrubasha/src/main/java/com/g2l/mathrubasha/lib/ToastMessage.java
+++ mathrubasha/src/main/java/com/g2l/mathrubasha/lib/ToastMessage.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToastMessage {
-    public static  void s(Context context,String str, Callback c) {
+    public static void s(Context context,String str, ResponseListener c) {
         Toast.makeText(context, str, Toast.LENGTH_LONG).show();
         c.onReceive("Hi people");
     }
