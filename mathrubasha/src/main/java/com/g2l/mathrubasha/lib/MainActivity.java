c9c014e2f11c0a0a3a162caa8a7347553cfe0983
khushi1706 <git-commit-address>
2020-12-29 22:07:01 +0530

    fourth commit

--- mathrubasha/src/main/java/com/g2l/mathrubasha/lib/MainActivity.java
+++ mathrubasha/src/main/java/com/g2l/mathrubasha/lib/MainActivity.java
@@ -2,13 +2,20 @@ package com.g2l.mathrubasha.lib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = getIntent();
+        String name = i.getStringExtra("TITLE");
+        tv = findViewById(R.id.hello_world);
+        tv.setText(name);
     }
 }
